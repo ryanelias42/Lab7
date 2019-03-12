025307901c3ed35eafd13368ca472aa00ba28383
Ryan E. <elias.ryan.matthew@gmail.com>
2019-03-12 16:25:24 -0500

    Moved TODO into project

---
+++ lab 7/Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-12
+OU ID_______________: 113481704
+OU EMAIL____________: ryanelias@ou.edu
+
+Tasks:
+1. complete Person child classes
+2. complete StackHospital class
+3. complete QueueHospital Class
+4. complete PriorityQueueHospital class
+5. complete person child class test methods
+6. complete Hospital child class test methods
+
+Estimated Time (minutes):
+1. 20
+2. 45
+3. 15
+4. 45
+5. 30
+6. 30
+
+Actual Time (minutes):
+1. 10
+2. 30
+3. 10
+4. 60
+5. 45
+6. 45
+*/}
