ab96a191e4e6200578c2defb718b2e1a0609a2e7
Ryan E. <elias.ryan.matthew@gmail.com>
2019-03-12 08:01:35 -0500

    Finished HealthyPerson.

--- lab 7/src/HealthyPerson.java
+++ lab 7/src/HealthyPerson.java
@@ -7,14 +7,44 @@ public class HealthyPerson extends Person{
 		super(name, age);
 		this.reason = reason;
 	}
+	
+	public String getReason() {
+		return reason;
+	}
 
+	
+	
+	/**
+	 * Subclass implementation of Person comparison.
+	 *
+	 * @param p The other person to compare to.
+	 * @return Standard Comparable values:
+	 * 	(1) self comes "before" p: negative number
+	 *  (2) self comes "after" p: positive number
+	 *  (3) self and p are equivalent in ordering: 0
+	 */
 	@Override
 	protected int compareToImpl(Person p) {
-		
+		if (p instanceof HealthyPerson) {
+			if (this.getName().compareTo(p.getName()) < 0) {
+				//if self name is lower value it is alphabetically first and is therefor returning a -1
+				return -1;
+			}
+			else if(this.getName().compareTo(p.getName()) > 0) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+		//if this is being compared to SickPerson, return 1 putting self afterwords in order or precedence to remain consistent. 
+		else {
+			return 1;
+		}
 	}
 	
 	public String toString() {
-		
+		return String.format("%s, a %d-year old. Reason for visit: %s.", this.getName(), this.getAge(), this.getReason());
 	}
 
 }
